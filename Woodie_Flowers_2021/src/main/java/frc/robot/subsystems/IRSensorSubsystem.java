b27ccffc68117c0a3f28a8aeb6910744d69426a4
Tyler <tylernewton08@gmail.com>
2021-02-25 19:36:01 -0500

    UltrasonicIRSensor in progress

---
+++ Woodie_Flowers_2021/src/main/java/frc/robot/subsystems/IRSensorSubsystem.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.DriveCommand;
+
+public class IRSensorSubsystem extends SubsystemBase {
+    private AnalogInput irAnalog = new AnalogInput(1);
+
+    public void IRSensorSubsytem() {
+
+    }
+
+    // Gets raw voltage
+
+    public double getVoltage() {
+        return irAnalog.getVoltage();
+    }
+
+    // Math to convert voltage to distance
+
+    public double getDistance() {
+        return getVoltage();
+
+    }
+}
+
+// Command to compute whether robot is looking at red or blue path
+
+// SHARP 7Y 2Y0A710 F <-- IR Sensor Model
+
+// Voltage per (1/cm) = 137.5x + 1.125
