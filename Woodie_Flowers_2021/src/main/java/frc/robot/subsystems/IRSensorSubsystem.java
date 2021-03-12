5dcb30ba81d7d3290d7d7f3563cc3019ba215d32
Tyler <tylernewton08@gmail.com>
2021-03-11 19:04:21 -0500

    Voltage to distance equation added

--- Woodie_Flowers_2021/src/main/java/frc/robot/subsystems/IRSensorSubsystem.java
+++ Woodie_Flowers_2021/src/main/java/frc/robot/subsystems/IRSensorSubsystem.java
@@ -11,7 +11,7 @@ public class IRSensorSubsystem extends SubsystemBase {
 
     }
 
-    // Gets raw voltage
+    // Gets raw voltage     
 
     public double getVoltage() {
         return irAnalog.getVoltage();
@@ -20,7 +20,7 @@ public class IRSensorSubsystem extends SubsystemBase {
     // Math to convert voltage to distance
 
     public double getDistance() {
-        return getVoltage();
+        return 137.5/(getVoltage() - 1.125);
 
     }
 }
@@ -29,4 +29,6 @@ public class IRSensorSubsystem extends SubsystemBase {
 
 // SHARP 7Y 2Y0A710 F <-- IR Sensor Model
 
-// Voltage per (1/cm) = 137.5x + 1.125
+// Voltage per (1/cm) = 137.5d + 1.125 (Equation for IR Sensor)
+
+// d = 137.5/(V - 1.125)
